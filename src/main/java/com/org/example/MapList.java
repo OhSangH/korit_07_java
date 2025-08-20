d400bec39059b89af44dc0cff61835fe65f0c117
OhSangH <dhtkdgus126@gmail.com>
2025-08-20 09:29:24 +0900

    feat: 20250820 review

--- src/main/java/com/org/example/MapList.java
+++ src/main/java/com/org/example/MapList.java
@@ -4,13 +4,14 @@ import java.util.*;
 
 public class MapList {
     public static void main(String[] args) {
-        List<Map<String, Object>> costumers = new ArrayList<Map<String, Object>>();
-        Map<String, Object> costumersMap1 = new HashMap<String, Object>();
+        List<Map<String, Object>> costumers = new ArrayList<>();
+        // 처음 Generic이 도입됬을 때는 선언할 때 선언문 및 생성자 둘다 타입을 명시 해 줘야 됬다.
+        Map<String, Object> costumersMap1 = new HashMap<>();
         costumersMap1.put("name", "홍길동");
         costumersMap1.put("rating", "vip");
         costumersMap1.put("age", 30);
 
-        Map<String, Object> costumersMap2 = new HashMap<String, Object>();
+        Map<String, Object> costumersMap2 = new HashMap<>();
         costumersMap2.put("name", "김기영");
         costumersMap2.put("rating", "gold");
         costumersMap2.put("age", 35);
